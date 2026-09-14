7ace40b3aa53e780cb8f4df934de8735327f04e0
danield912 <tur36985@temple.edu>
2026-09-14 12:30:03 -0400

    testing if works

---
+++ test_neo4j.py
@@ -0,0 +1,20 @@
+import os
+from dotenv import load_dotenv
+from neo4j import GraphDatabase
+
+load_dotenv()
+
+uri = os.getenv("NEO4J_URI")
+user = os.getenv("NEO4J_USER")
+password = os.getenv("NEO4J_PASSWORD")
+
+driver = GraphDatabase.driver(
+    uri,
+    auth=(user, password)
+)
+
+try:
+    driver.verify_connectivity()
+    print("SUCCESS: Connected to Neo4j!")
+finally:
+    driver.close()
